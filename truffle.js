2d6c9669025e257ba28eae6a6bfe496bac62eef5
ConsenSys Academy <41963038+ConsenSys-Academy@users.noreply.github.com>
2018-12-19 11:40:42 -0500

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,3 +1,10 @@
+/*
+
+Solidity compiler version 0.4.25 is secified as Truffle v5.0 comes with compiler version 0.5.
+Feel free to use Solidity version 0.5
+
+*/
+
 module.exports = {
   compilers: {
     solc: '0.4.25'
