0b376b40d8035b2c368b038de75af3efab5aff1f
cds-amal <cds.sudama@gmail.com>
2021-09-08 13:37:17 -0400

    Open tests for student grading

--- test/supply_chain.test.js
+++ test/supply_chain.test.js
@@ -18,15 +18,15 @@ contract("SupplyChain", function (accounts) {
   });
 
   describe("Variables", () => {
-    it.skip("should have an owner", async () => {
+    it("should have an owner", async () => {
       assert.equal(typeof instance.owner, 'function', "the contract has no owner");
     });
 
-    it.skip("should have an skuCount", async () => {
+    it("should have an skuCount", async () => {
       assert.equal(typeof instance.skuCount, 'function', "the contract has no skuCount");
     });
 
-    describe.skip("enum State", () => {
+    describe("enum State", () => {
       let enumState;
       before(() => {
         enumState = SupplyChain.enums.State;
@@ -36,28 +36,28 @@ contract("SupplyChain", function (accounts) {
         );
       });
 
-      it.skip("should define `ForSale`", () => {
+      it("should define `ForSale`", () => {
         assert(
           enumState.hasOwnProperty('ForSale'),
           "The enum does not have a `ForSale` value"
         );
       });
 
-      it.skip("should define `Sold`", () => {
+      it("should define `Sold`", () => {
         assert(
           enumState.hasOwnProperty('Sold'),
           "The enum does not have a `Sold` value"
         );
       });
 
-      it.skip("should define `Shipped`", () => {
+      it("should define `Shipped`", () => {
         assert(
           enumState.hasOwnProperty('Shipped'),
           "The enum does not have a `Shipped` value"
         );
       });
 
-      it.skip("should define `Received`", () => {
+      it("should define `Received`", () => {
         assert(
           enumState.hasOwnProperty('Received'),
           "The enum does not have a `Received` value"
@@ -65,7 +65,7 @@ contract("SupplyChain", function (accounts) {
       });
     })
 
-    describe.skip("Item struct", () => {
+    describe("Item struct", () => {
       let subjectStruct;
 
       before(() => {
@@ -76,7 +76,7 @@ contract("SupplyChain", function (accounts) {
         );
       });
 
-      it.skip("should have a `name`", () => {
+      it("should have a `name`", () => {
         assert(
           isDefined(subjectStruct)("name"), 
           "Struct Item should have a `name` member"
@@ -87,7 +87,7 @@ contract("SupplyChain", function (accounts) {
         );
       });
 
-      it.skip("should have a `sku`", () => {
+      it("should have a `sku`", () => {
         assert(
           isDefined(subjectStruct)("sku"), 
           "Struct Item should have a `sku` member"
@@ -98,7 +98,7 @@ contract("SupplyChain", function (accounts) {
         );
       });
 
-      it.skip("should have a `price`", () => {
+      it("should have a `price`", () => {
         assert(
           isDefined(subjectStruct)("price"), 
           "Struct Item should have a `price` member"
@@ -109,7 +109,7 @@ contract("SupplyChain", function (accounts) {
         );
       });
 
-      it.skip("should have a `state`", () => {
+      it("should have a `state`", () => {
         assert(
           isDefined(subjectStruct)("state"), 
           "Struct Item should have a `state` member"
@@ -120,7 +120,7 @@ contract("SupplyChain", function (accounts) {
         );
       });
 
-      it.skip("should have a `seller`", () => {
+      it("should have a `seller`", () => {
         assert(
           isDefined(subjectStruct)("seller"), 
           "Struct Item should have a `seller` member"
@@ -135,7 +135,7 @@ contract("SupplyChain", function (accounts) {
         );
       });
 
-      it.skip("should have a `buyer`", () => {
+      it("should have a `buyer`", () => {
         assert(
           isDefined(subjectStruct)("buyer"), 
           "Struct Item should have a `buyer` member"
@@ -153,7 +153,7 @@ contract("SupplyChain", function (accounts) {
   });
 
   describe("Use cases", () => {
-    it.skip("should add an item with the provided name and price", async () => {
+    it("should add an item with the provided name and price", async () => {
       await instance.addItem(name, price, { from: alice });
 
       const result = await instance.fetchItem.call(0);
@@ -185,7 +185,7 @@ contract("SupplyChain", function (accounts) {
       );
     });
 
-    it.skip("should emit a LogForSale event when an item is added", async () => {
+    it("should emit a LogForSale event when an item is added", async () => {
       let eventEmitted = false;
       const tx = await instance.addItem(name, price, { from: alice });
 
@@ -200,7 +200,7 @@ contract("SupplyChain", function (accounts) {
       );
     });
 
-    it.skip("should allow someone to purchase an item and update state accordingly", async () => {
+    it("should allow someone to purchase an item and update state accordingly", async () => {
       await instance.addItem(name, price, { from: alice });
       var aliceBalanceBefore = await web3.eth.getBalance(alice);
       var bobBalanceBefore = await web3.eth.getBalance(bob);
@@ -237,12 +237,12 @@ contract("SupplyChain", function (accounts) {
       );
     });
 
-    it.skip("should error when not enough value is sent when purchasing an item", async () => {
+    it("should error when not enough value is sent when purchasing an item", async () => {
       await instance.addItem(name, price, { from: alice });
       await catchRevert(instance.buyItem(0, { from: bob, value: 1 }));
     });
 
-    it.skip("should emit LogSold event when and item is purchased", async () => {
+    it("should emit LogSold event when and item is purchased", async () => {
       var eventEmitted = false;
 
       await instance.addItem(name, price, { from: alice });
@@ -255,13 +255,13 @@ contract("SupplyChain", function (accounts) {
       assert.equal(eventEmitted, true, "adding an item should emit a Sold event");
     });
 
-    it.skip("should revert when someone that is not the seller tries to call shipItem()", async () => {
+    it("should revert when someone that is not the seller tries to call shipItem()", async () => {
       await instance.addItem(name, price, { from: alice });
       await instance.buyItem(0, { from: bob, value: price });
       await catchRevert(instance.shipItem(0, { from: bob }));
     });
 
-    it.skip("should allow the seller to mark the item as shipped", async () => {
+    it("should allow the seller to mark the item as shipped", async () => {
       await instance.addItem(name, price, { from: alice });
       await instance.buyItem(0, { from: bob, value: excessAmount });
       await instance.shipItem(0, { from: alice });
@@ -275,7 +275,7 @@ contract("SupplyChain", function (accounts) {
       );
     });
 
-    it.skip("should emit a LogShipped event when an item is shipped", async () => {
+    it("should emit a LogShipped event when an item is shipped", async () => {
       var eventEmitted = false;
 
       await instance.addItem(name, price, { from: alice });
@@ -293,7 +293,7 @@ contract("SupplyChain", function (accounts) {
       );
     });
 
-    it.skip("should allow the buyer to mark the item as received", async () => {
+    it("should allow the buyer to mark the item as received", async () => {
       await instance.addItem(name, price, { from: alice });
       await instance.buyItem(0, { from: bob, value: excessAmount });
       await instance.shipItem(0, { from: alice });
@@ -308,7 +308,7 @@ contract("SupplyChain", function (accounts) {
       );
     });
 
-    it.skip("should revert if an address other than the buyer calls receiveItem()", async () => {
+    it("should revert if an address other than the buyer calls receiveItem()", async () => {
       await instance.addItem(name, price, { from: alice });
       await instance.buyItem(0, { from: bob, value: excessAmount });
       await instance.shipItem(0, { from: alice });
@@ -316,7 +316,7 @@ contract("SupplyChain", function (accounts) {
       await catchRevert(instance.receiveItem(0, { from: alice }));
     });
 
-    it.skip("should emit a LogReceived event when an item is received", async () => {
+    it("should emit a LogReceived event when an item is received", async () => {
       var eventEmitted = false;
 
       await instance.addItem(name, price, { from: alice });
